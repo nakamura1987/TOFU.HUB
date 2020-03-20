511b7324132a98fac0d92efc566f2c79f2d26e61
nakamura1987 <riserva.chianti@gmail.com>
2020-03-20 18:07:53 +0900

    add form

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,2 +1,17 @@
 class ShopsController < ApplicationController
+  def new
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    @shop.save
+    redirect_to @shop
+  end
+
+  private
+  def post_params
+    params.require(:shop).permit(:store_name, :address, :phone_number, :open_hour, :image)
+  end
+
 end
